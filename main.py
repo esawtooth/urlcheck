8f4520922d42b60d5c5caf8e99b5cb08a882dca1
Rohit Jain <rohit@aws.rohitja.in>
2015-12-14 10:09:51 +0000

    more fixes

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ import flask
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
+fileName = "urllist"
 # app.config['SERVER_NAME'] = "aws.rohitja.in"
 
 @app.route("/geturl", methods = ["POST"])
 def getUrl():
-    urlRequested = flask.request.form['url']
-    info = getUrlAvailability(urlRequested)
+    url = flask.request.form['url']
+    email = flask.request.form['email']
+    info = getUrlAvailability(url)
     if info:
-        print urlRequested, info
-        return "The requested url is not available. It will expire on " + str(info)
+        logRequest(url, email, info)
+        print url, email, info
+        return "The requested url is not available. It will expire on " + str(info) + ". We will email you when it is available."
     else:
         return "Contratulations: The requested url is available."
 
@@ -24,6 +27,8 @@ def front():
 def getUrlAvailability(url):
     w = whois.whois(url)
     return w.expiration_date
-
+def logRequest(url, email, info):
+    with open(fileName, 'a') as f:
+        f.write(str(url) + "|" + str(email) + str(info) + "\n" + "----------" + "\n")
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
