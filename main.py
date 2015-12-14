b4b2e4e8712418ba3552ab1e22a0c464733678ee
Rohit Jain <rohit@aws.rohitja.in>
2015-12-14 09:55:50 +0000

    Some basic code for getting URL info

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+#!/usr/bin/env python
+
+import whois
+import flask
 from flask import Flask
 app = Flask(__name__)
+app.config['DEBUG'] = True
+# app.config['SERVER_NAME'] = "aws.rohitja.in"
+
+@app.route("/geturl", methods = ["POST"])
+def getUrl():
+    urlRequested = flask.request.form['url']
+    info = getUrlAvailability(urlRequested)
+    if info:
+        print urlRequested, info
+        return "The requested url is not available. It will expire on " + str(info)
+    else:
+        return "Contratulations: The requested url is available."
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def front():
+    return flask.render_template("./index.html")
 
-def getUrlInfo(url):
-    
-
+def getUrlAvailability(url):
+    w = whois.whois(url)
+    return w.expiration_date
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
